60c65f76378ea12627f1c0f1059c8567dda19049
jhonjonh <john.gerber@live.com>
2021-09-28 18:05:52 -0300

    implementada rota checked

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -25,4 +25,11 @@ export class ListarTarefaComponent implements OnInit {
     return this.tarefaService.listarTodos();
   }
 
+  alterarStatus(tarefa:Tarefa) : void {
+    if(confirm('Deseja alterar o status da tarefa " '+ tarefa.nome +' "? ')){
+      this.tarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
+  }
+
 }
