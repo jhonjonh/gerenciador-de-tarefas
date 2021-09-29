e24259798ff937bae71691419734e6b8748d6a57
jhonjonh <john.gerber@live.com>
2021-09-29 09:54:48 -0300

    implantada rota excluir

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -29,7 +29,20 @@ export class ListarTarefaComponent implements OnInit {
     if(confirm('Deseja alterar o status da tarefa " '+ tarefa.nome +' "? ')){
       this.tarefaService.alterarStatus(tarefa.id);
       this.tarefas = this.listarTodos();
+    } else {
+      this.tarefas = this.listarTodos();
+    }
+  }
+
+  remover($event:any, tarefa:Tarefa) :void {
+    $event.preventDefault();
+    if(confirm('Deseja remover a tarefa " '+ tarefa.nome +' "? ')){
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.listarTodos();
+    } else {
+      this.tarefas = this.listarTodos();
     }
   }
 
 }
+
